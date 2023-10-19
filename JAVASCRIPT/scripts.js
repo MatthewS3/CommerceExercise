d49d76b0ad9d4288bf5591cc17f93d40dd62661f
MatthewS3 <mtthwswain@gmail.com>
2023-10-19 15:49:47 +0200

    moreupdates

--- JAVASCRIPT/scripts.js
+++
@@ -1,8 +0,0 @@
-function startAnimation() {
-    const box = document.querySelector('.box');
-    if (box.style.animationPlayState === 'paused') {
-        box.style.animationPlayState = 'running';
-    } else {
-        box.style.animationPlayState = 'paused';
-    }
-}
